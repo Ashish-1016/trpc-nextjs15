f6e0379838b7617eb06e7f06cf1b43bc0cbd3d61
ashishjeswani <ashish.jeswani@noovosoft.com>
2025-02-15 03:00:06 +0530

    start work on generic form schema, add shadcn components

--- app/main-layout.tsx
+++ app/main-layout.tsx
@@ -1,18 +1,10 @@
 import { ReactNode } from "react";
 import { MainBodySection } from "@/app/_components/MainBodySection";
-import { Header } from "@/app/_components/Header";
 
 export const MainLayout = ({ children }: { children: ReactNode }) => {
-
   return (
-    <div className="h-dvh w-full flex flex-col items-center justify-center">
-
-      {/*<Header />*/}
-
-      <MainBodySection>
-        {children}
-      </MainBodySection>
-
-    </div>
-  )
-}
+    <main className="h-dvh w-full flex flex-col items-center justify-center">
+      <MainBodySection>{children}</MainBodySection>
+    </main>
+  );
+};
